1ea3068db68bec74e2845d8bc1c4b641573778c4
F4dhil54 <103545327+F4dhil54@users.noreply.github.com>
2022-06-14 13:39:10 +0700

    Add files via upload

---
+++ Laundry/Petugas.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laundry;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Petugas implements User{ //polimerism
+    private ArrayList<String> namaPetugas = new ArrayList<String>();
+    private ArrayList<String> alamat = new ArrayList<String>();
+    private ArrayList<String> telepon = new ArrayList<String>();
+    
+    public Petugas(){
+        this.namaPetugas.add("Administrator X");//data terkini di dalam kelas sendiri
+        this.alamat.add("Mars");
+        this.telepon.add("089********");
+        
+        this.namaPetugas.add("Administrator Y");
+        this.alamat.add("Jupiter");
+        this.telepon.add("081*********");
+    }
+     
+    //inheritace
+    @Override
+    public void setNama(String nama) {
+        this.namaPetugas.add(nama);
+    }
+
+    @Override
+    public void setAlamat(String alamat) {
+        this.alamat.add(alamat);
+    }
+
+    @Override
+    public void setTelepon(String telepon) {
+        this.telepon.add(telepon);
+    }
+
+    @Override
+    public String getNama(int id) {
+        return this.namaPetugas.get(id);
+    }
+
+    @Override
+    public String getAlamat(int id) {
+        return this.alamat.get(id);
+    }
+
+    @Override
+    public String getTelepon(int id) {
+        return this.telepon.get(id);
+    }
+    
+    public void tampilPetugas(){
+        int n = this.namaPetugas.size();
+        for(int i=0;i<n;i++){
+            System.out.println("------------------------");
+            System.out.println("Nama    = "+getNama(i));
+            System.out.println("Alamat  = "+getAlamat(i));
+            System.out.println("Telepon = "+getTelepon(i));
+        }
+    }
+}
+
+
+    
+
